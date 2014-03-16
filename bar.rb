35d10f3a771c58d96265f2361cfa6fa00a8ad1bb
Ronak Patel <patel66@sbcglobal.net>
2014-03-16 05:29:13 +0000

    Happy discount has a default value of zero

--- bar.rb
+++ bar.rb
@@ -2,6 +2,7 @@ require 'time' # you're gonna need it
 
 class Bar
   attr_reader :name
+  attr_writer :happy_discount
   attr_accessor :menu_items
 
   def initialize(name)
@@ -11,6 +12,10 @@ class Bar
 
   def add_menu_item(item, price)
     menu_items.push(Item.new(item, price))
+  end
+
+  def happy_discount
+    @happy_discount || 0
   end
 
 end
