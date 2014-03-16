0a4dfa8629fa6e7c979e61c49007d2b173518baf
Ronak Patel <patel66@sbcglobal.net>
2014-03-16 05:23:07 +0000

    bar initializes with menu item

--- bar.rb
+++ bar.rb
@@ -2,7 +2,24 @@ require 'time' # you're gonna need it
 
 class Bar
   attr_reader :name
+  attr_accessor :menu_items
+
   def initialize(name)
     @name = name
+    @menu_items = []
   end
+
+  def add_menu_item(item, price)
+    menu_items.push(Item.new(item, price))
+  end
+
+end
+
+class Item
+
+  def initialize(item, price)
+    @item = item
+    @price = price
+  end
+
 end
